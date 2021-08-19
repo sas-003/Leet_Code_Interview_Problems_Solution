8250afef996a13ea18af26dc69309c6d290fd0d7
Saiful Alam <SAIFUL_ALAM@Outlook.com>
2021-08-18 22:34:45 -0500

    Add files via upload

---
+++ perfectNumber.py
@@ -0,0 +1,13 @@
+def perfectSum(num):
+	sum = 0
+	for i in range ( 1, num,1 ):
+		if (num % i == 0):
+			sum = sum + i
+	if (num * 2 == sum):
+		print ( "perfect num" )
+	else:
+		print ( 'Not a perfect Numbers' )
+
+
+number = int ( input ( "please enter a number\t" ) )
+perfectSum ( number )
--- reverse_int.py
+++ reverse_int.py
@@ -1,11 +1,9 @@
-def reverse_integer(n):
-   y = str(abs(n))
-   y = y.strip()
-   y = y[::-1]
-   output = int(y)
-   if output >= 2** 31 -1 or output <= -2** 31:
-       return 0
-   elif n < 0:
-      return -1 * output
-   else:
-      return output
+def reverseInt(n):
+	reverse=0
+	while(n>0):
+		reverse=reverse*10 + n%10
+		n=n//10
+
+	return reverse
+number=int(input("Please enter a number you want to reverse "))
+print(reverseInt(number))
